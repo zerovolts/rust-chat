7700ee1cf329b5fae93b9e19d531fb92a55c9ebc
Zach Stone <hexaphixylix@yahoo.com>
2017-12-24 15:16:55 -0500

    add simple client

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
-use std::io::Read;
+use std::io::{Read, Write};
+use std::{thread, time, env};
 
 fn handle_client(mut stream: Arc<Mutex<TcpStream>>) {
     let mut buffer = String::new();
@@ -9,6 +10,17 @@ fn handle_client(mut stream: Arc<Mutex<TcpStream>>) {
 }
 
 fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 && args[1] == "server" {
+        println!("Starting server...");
+        server();
+    } else {
+        println!("Starting client...");
+        client();
+    }
+}
+
+fn server() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let mut connections: Vec<Arc<Mutex<TcpStream>>> = Vec::new();
     let mut messages: Vec<String> = Vec::new();
@@ -24,4 +36,13 @@ fn main() {
             Err(e) => ()
         }
     }
+}
+
+fn client() {
+    let mut stream = TcpStream::connect("127.0.0.1:8080")
+        .expect("Couldn't connect to the server.");
+
+    let message = String::from("hello there!");
+    let _ = stream.write(message.as_bytes());
+    println!("sent");
 }
